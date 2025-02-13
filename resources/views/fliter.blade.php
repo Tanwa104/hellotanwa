d66e0c01350de468d9f1b4963d5e7de847325fee
unknown <tanwanishaan@gmail.com>
2025-02-13 18:33:33 +0530

    comments

--- resources/views/fliter.blade.php
+++ resources/views/fliter.blade.php
@@ -1,124 +1,88 @@
-<html>
-    <head>
-        <title>find new Clients</title>
-
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    </script>
-    </head>
-    <body>
-        
-            <form class="row g-3" action="{{route('fliter.build')}}" method="get">
-                
-                @if (\Session::has('msg'))
-    <div class="alert alert-success">
-        <ul>
-            <li>{!! \Session::get('msg') !!}</li>
-        </ul>
-    </div>
-@endif
-<div class="col-md-6">
-    @foreach($items as $item)
-    <label for="name" class="form-label">Name </label>
-    <input type="text" class="form-control" id="name" name="name" value="{{$item->name}}"/>
-    <label for="lastname" class="form-label">lastname </label>
-    <input type="text" class="form-control" id="lastname" name="lastname" value="{{$item->lastname}}"/>
-    @endforeach
-</div>
-                <div class="col-md-6">
-                  <label for="inputEmail4" class="form-label">enter the time for work</label>
-                  <span id="inputEmail4" class="form-control">
-                  <select name="bhours">
+@extends('layout.master')
+@section('container')
+<div class="container-xxl bg-white p-0">
+    <div class="container-xxl py-5 bg-dark hero-header mb-5">
+        <div class="container my-5 py-5">
+            <div class="row align-items-center g-5">
+                <div class="col-lg-6 text-center text-lg-start">
+                    <h1 class="display-3 text-white animated slideInLeft">Karvae<br>Ghar ki seva</h1>
+                    <p class="text-white animated slideInLeft mb-4 pb-2">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet</p>
                     
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                    <option value="6">6</option>
-                    <option value="7">7</option>
-                    <option value="8">8</option>
-                    <option value="9">9</option>
-                    <option value="10">10</option>
-                    <option value="11">11</option>
-                    <option value="12">12</option>
-                   </select>
-                   <select name="bmin" >
-                    <option value=":00">:00</option> 
-                    <option value=":15">:15</option>
-                    <option value=":30">:30</option>
-                    <option value=":45">:45</option>
-                   </select>
-                   <select name="bampm">
-                    <option value="am">am</option> 
-                    <option value="pm">pm</option>
-                    </select> to <select name="ahours">
-                     <option value="1">1</option>
-                     <option value="2">2</option>
-                     <option value="3">3</option>
-                     <option value="4">4</option>
-                     <option value="5">5</option>
-                     <option value="6">6</option>
-                     <option value="7">7</option>
-                     <option value="8">8</option>
-                     <option value="9">9</option>
-                     <option value="10">10</option>
-                     <option value="11">11</option>
-                     <option value="12">12</option>
-                    </select>
-                    <select name="amins">
-                     <option value=":00">:00</option> 
-                     <option value=":15">:15</option>
-                     <option value=":30">:30</option>
-                     <option value=":45">:45</option>
-                    </select>
-                    <select name="aampm">
-                     <option value="am">am</option> 
-                     <option value="pm">pm</option>
-                    </select>
-                    </span>
-        <br><br>
                 </div>
-                
-
-            
-                <div class="col-md-6">
-            <label for="inputEmail5" class="form-label">enter number of days for work</label>
-            <span id="inputEmail5" class="form-control">
-            <input type="checkbox" class="btn-check" id="btn-check-4" autocomplete="off" name="weekdays[]" value="monday">
-<label class="btn" for="btn-check-4">Monday</label><br>
-<input type="checkbox" class="btn-check" id="btn-check-5" autocomplete="off" name="weekdays[]" value="tuesday">
-<label class="btn" for="btn-check-5">Tuesday</label><br>
-<input type="checkbox" class="btn-check" id="btn-check-6" autocomplete="off" name="weekdays[]" value="wednesday">
-<label class="btn" for="btn-check-6">Wednesday</label><br>
-<input type="checkbox" class="btn-check" id="btn-check-7" autocomplete="off" name="weekdays[]" value="thursday">
-<label class="btn" for="btn-check-7">Thursday</label><br>
-<input type="checkbox" class="btn-check" id="btn-check-8" autocomplete="off" name="weekdays[]" value="friday">
-<label class="btn" for="btn-check-8">Friday</label><br>
-<input type="checkbox" class="btn-check" id="btn-check-9" autocomplete="off" name="weekdays[]" value="saturday">
-<label class="btn" for="btn-check-9">Saturday</label><br>
-<input type="checkbox" class="btn-check" id="btn-check-10" autocomplete="off" name="weekdays[]" value="sunday">
-<label class="btn" for="btn-check-10">Sunday</label><br><br>
-            </span>
-          </div>
-          <div class="col-md-6">
-            <label for="inputEmail6" class="form-label">want to work for </label>
-            <span id="inputEmail6" class="form-control">
-                <input type="radio" class="btn-check" name="options" id="singletime" autocomplete="off" value="singletime">
-                <label class="btn btn-outline-danger" for="singletime">Singletime</label>&nbsp;&nbsp;
-                
-                or&nbsp;&nbsp;
-                
-                <input type="radio" class="btn-check" name="options" id="fulltime" autocomplete="off" value="fulltime">
-                <label class="btn btn-outline-danger" for="fulltime">Fulltime</label>
-             </span></div></div>
-<input type="submit" value="confirm">
+                <div class="col-lg-6 text-center text-lg-end overflow-hidden">
+                    <img class="img-fluid" src="img/hero.png" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 
-<a href="{{route('user.build')}}" class="btn btn-primary col-md-3">Back</a>     
+        <!-- Contact Start -->
+        <div class="container-xxl py-5">
+            <div class="container">
+                <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
+                    <h5 class="section-title ff-secondary text-center text-primary fw-normal">please</h5>
+                    <h1 class="mb-5">Enter the given details</h1>
+                </div>
+                <div class="row g-4">
+                    <div class="col-12">
+                        <div class="row gy-4">
+                            
+                        </div>
+                    </div>
+                    <div class="col-md-6 wow fadeIn" data-wow-delay="0.1s">
+                        <iframe class="position-relative rounded w-100 h-100"
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3001156.4288297426!2d-78.01371936852176!3d42.72876761954724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4ccc4bf0f123a5a9%3A0xddcfc6c1de189567!2sNew%20York%2C%20USA!5e0!3m2!1sen!2sbd!4v1603794290143!5m2!1sen!2sbd"
+                            frameborder="0" style="min-height: 350px; border:0;" allowfullscreen="" aria-hidden="false"
+                            tabindex="0"></iframe>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="wow fadeInUp" data-wow-delay="0.2s">
+                            <form>
+                                <div class="row g-3">
+                                    <div class="col-md-6">
+                                        <div class="form-floating">
+                                            {{-- <input type="text" class="form-control" id="name" placeholder="Your Name"> --}}
+                                            {{-- <label for="name">Your Name</label> --}}
+                                            @foreach($items as $item)
+                                            
+                                            <input type="text" class="form-control" id="name" name="name" value="{{$item->name}}"/>
+                                            <label for="name" class="form-label">Name </label>
+                                            @endforeach
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-floating">
+                                            @foreach($items as $item)
+                                           
+                                            <input type="text" class="form-control" id="lastname" name="lastname" value="{{$item->lastname}}"/>
+                                            <label for="lastname" class="form-label">lastname </label>
+                                            @endforeach
+                                        </div>
+                                    </div>
+                                    <div class="col-12">
+                                        <div class="form-floating">
+                                            <input type="text" class="form-control" id="subject" placeholder="Subject">
+                                            <label for="subject">Subject</label>
+                                        </div>
+                                    </div>
+                                    <div class="col-12">
+                                        <div class="form-floating">
+                                            <textarea class="form-control" placeholder="Leave a message here" id="message" style="height: 150px"></textarea>
+                                            <label for="message">Message</label>
+                                        </div>
+                                    </div>
+                                    <div class="col-12">
+                                        <button class="btn btn-primary w-100 py-3" type="submit">Send Message</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Contact End -->
 
-        </form>
-    </body>
-</html>
+@endsection
--- resources/views/user_dash.blade.php
+++ resources/views/user_dash.blade.php
@@ -5,9 +5,9 @@
             <div class="container my-5 py-5">
                 <div class="row align-items-center g-5">
                     <div class="col-lg-6 text-center text-lg-start">
-                        <h1 class="display-3 text-white animated slideInLeft">Enjoy Our<br>Delicious Meal</h1>
+                        <h1 class="display-3 text-white animated slideInLeft">Karvae<br>Ghar ki seva</h1>
                         <p class="text-white animated slideInLeft mb-4 pb-2">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet</p>
-                        <a href="" class="btn btn-primary py-sm-3 px-sm-5 me-3 animated slideInLeft">Book A Table</a>
+                        
                     </div>
                     <div class="col-lg-6 text-center text-lg-end overflow-hidden">
                         <img class="img-fluid" src="img/hero.png" alt="">
@@ -34,78 +34,59 @@
         <div class="container-xxl pt-5 pb-3">
             <div class="container">
                 <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
-                    <h5 class="section-title ff-secondary text-center text-primary fw-normal">Team Members</h5>
-                    <h1 class="mb-5">Our Master Chefs</h1>
+                    <h5 class="section-title ff-secondary text-center text-primary fw-normal">services</h5>
+                    <h1 class="mb-5">Available</h1>
                 </div>
                 <div class="row g-4">
                     <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
                         <div class="team-item text-center rounded overflow-hidden">
                             <div class="rounded-circle overflow-hidden m-4">
-                                <img class="img-fluid" src="img/team-1.jpg" alt="">
+                                {{-- <img class="img-fluid" src="img/team-1.jpg" alt=""> --}}
+                                <form action="{{ route('fliter') }}" method="get">
+                                    <input type="hidden" id="role" name="role" value="Housecleaner">
+                                    <input type="image" class="img-fluid" src="/img/gharclean.png" alt="Submit"
+                                     >
                             </div>
-                            <h5 class="mb-0">Full Name</h5>
+                            <h5 class="mb-0">Housecleaning</h5>
                             <small>Designation</small>
                             <div class="d-flex justify-content-center mt-3">
-                                <a class="btn btn-square btn-primary mx-1" href=""><i
-                                        class="fab fa-facebook-f"></i></a>
-                                <a class="btn btn-square btn-primary mx-1" href=""><i
-                                        class="fab fa-twitter"></i></a>
-                                <a class="btn btn-square btn-primary mx-1" href=""><i
-                                        class="fab fa-instagram"></i></a>
+                                
                             </div>
                         </div>
                     </div>
                     <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
                         <div class="team-item text-center rounded overflow-hidden">
                             <div class="rounded-circle overflow-hidden m-4">
-                                <img class="img-fluid" src="img/team-2.jpg" alt="">
+                                {{-- <img class="img-fluid" src="img/team-2.jpg" alt=""> --}}
+                                <form action="{{ route('fliter') }}" method="get">
+                                    <input type="hidden" id="role" name="role" value="childcare">
+                                    <input type="image" class="img-fluid" src="/img/childnanny.png" alt="Submit"
+                                          >
                             </div>
-                            <h5 class="mb-0">Full Name</h5>
+                            <h5 class="mb-0">Childcare</h5>
                             <small>Designation</small>
                             <div class="d-flex justify-content-center mt-3">
-                                <a class="btn btn-square btn-primary mx-1" href=""><i
-                                        class="fab fa-facebook-f"></i></a>
-                                <a class="btn btn-square btn-primary mx-1" href=""><i
-                                        class="fab fa-twitter"></i></a>
-                                <a class="btn btn-square btn-primary mx-1" href=""><i
-                                        class="fab fa-instagram"></i></a>
+                               
                             </div>
                         </div>
                     </div>
                     <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
                         <div class="team-item text-center rounded overflow-hidden">
                             <div class="rounded-circle overflow-hidden m-4">
-                                <img class="img-fluid" src="img/team-3.jpg" alt="">
+                                {{-- <img class="img-fluid" src="img/team-3.jpg" alt=""> --}}
+                                <form action="{{ route('fliter') }}" method="get">
+                                    <input type="hidden" id="role" name="role" value="houseCook">
+                                    <input type="image" class="img-fluid" src="/img/housecook.png" alt="Submit"
+                                     >
                             </div>
-                            <h5 class="mb-0">Full Name</h5>
+                            <h5 class="mb-0">Homecook</h5>
                             <small>Designation</small>
                             <div class="d-flex justify-content-center mt-3">
-                                <a class="btn btn-square btn-primary mx-1" href=""><i
-                                        class="fab fa-facebook-f"></i></a>
-                                <a class="btn btn-square btn-primary mx-1" href=""><i
-                                        class="fab fa-twitter"></i></a>
-                                <a class="btn btn-square btn-primary mx-1" href=""><i
-                                        class="fab fa-instagram"></i></a>
+                              
                             </div>
                         </div>
                     </div>
-                    <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.7s">
-                        <div class="team-item text-center rounded overflow-hidden">
-                            <div class="rounded-circle overflow-hidden m-4">
-                                <img class="img-fluid" src="img/team-4.jpg" alt="">
-                            </div>
-                            <h5 class="mb-0">Full Name</h5>
-                            <small>Designation</small>
-                            <div class="d-flex justify-content-center mt-3">
-                                <a class="btn btn-square btn-primary mx-1" href=""><i
-                                        class="fab fa-facebook-f"></i></a>
-                                <a class="btn btn-square btn-primary mx-1" href=""><i
-                                        class="fab fa-twitter"></i></a>
-                                <a class="btn btn-square btn-primary mx-1" href=""><i
-                                        class="fab fa-instagram"></i></a>
-                            </div>
-                        </div>
-                    </div>
+                </div>
                 </div>
             </div>
         </div>
