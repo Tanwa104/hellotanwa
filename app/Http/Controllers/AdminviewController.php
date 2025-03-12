4eee2ca5e6c053a30a93422a0ee7fda4dc5faf6f
unknown <tanwanishaan@gmail.com>
2025-03-12 17:30:46 +0530

    comments

--- app/Http/Controllers/AdminviewController.php
+++ app/Http/Controllers/AdminviewController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Helper;
+use App\Models\Booking;
 
 class AdminviewController extends Controller
 {
     public function view()
     {
-        return view('admin/dash');
+        $users = User::where('userrole', '=', 'user')->get();
+        $helpers=Helper::with('user')->get();
+        $bookings = Booking::with(['user', 'helper', 'timeline', 'userAddress'])  
+   
+    ->get();
+    
+
+        return view('admin/dash',compact('users','helpers','bookings'));
     }
 }
